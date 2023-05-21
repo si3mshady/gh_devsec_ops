12536ca38c4cc0ef3a6b6de7add55c839658b6d2
Elliott Arnold <si3mshady@gmail.com>
2023-05-21 04:18:17 -0500

    configuring elk upgraded instance m4.large

---
+++ log_generator.py
@@ -0,0 +1,23 @@
+import logging
+import sys
+import time
+from faker import Faker
+
+fake = Faker()
+
+# Configure logging
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger("log_generator")
+
+# Generate and log fake data
+while True:
+    log_data = {
+        "timestamp": fake.date_time_this_year(),
+        "message": fake.sentence(),
+        "source": fake.ipv4_public(),
+        "user_agent": fake.user_agent(),
+        "status_code": fake.random_int(min=200, max=500),
+    }
+
+    logger.info(log_data)
+    time.sleep(20)
